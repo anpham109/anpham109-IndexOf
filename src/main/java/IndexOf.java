d27133e782899a4a15ab2d677df8b8f6d3b05d90
Andy Pham <anpham-work@protonmail.com>
2023-07-21 15:58:06 +0000

    finished the challenge

--- src/main/java/IndexOf.java
+++ src/main/java/IndexOf.java
@@ -11,6 +11,13 @@ public class IndexOf {
      * @return the first index (position) of n in arr. if n does not exist in arr, return -1.
      */
     public int getIndex(int[] arr, int n){
+        int index = 0;
+        while(index < arr.length){
+            if(arr[index] == n){
+                return index;
+            }
+            index++;
+        }
         return -1;
     }
 }
